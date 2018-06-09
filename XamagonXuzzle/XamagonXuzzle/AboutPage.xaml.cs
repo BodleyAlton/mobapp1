77d1db1801bd7df5b36184ec20dd09d054595881
BodleyAlton <altonbodley@gmail.com>
2018-06-09 01:24:31 -0500

    Initial Commit

---
+++ XamagonXuzzle/XamagonXuzzle/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamagonXuzzle
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent ();
+        }
+    }
+}
---
+++ XamagonXuzzle/XamagonXuzzle/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamagonXuzzle
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent ();
+        }
+
+        async void OnButtonTap(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Hello There!", "Would you like to play a game?", "Yes", "No");
+            if (answer == true)
+            {
+                PuzzlePage puzzle = new PuzzlePage();
+                await Navigation.PushModalAsync(puzzle);
+            }
+        }
+    }
+}
